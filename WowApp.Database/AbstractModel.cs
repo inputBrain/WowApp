43f9c7690a425a5c150b6cb7f59eb90b0aff4dd8
Alexander <vsany@meta.ua>
2021-08-28 09:00:55 +0300

    + comments

--- WowApp.Database/AbstractModel.cs
+++ WowApp.Database/AbstractModel.cs
@@ -1,5 +1,7 @@
 namespace WowApp.Database
 {
+    // Абстракная моделька которую должны реализовать все модельки. Например, class UserModel : AbstractModel
+    // Это нужно для того, чтобы внеднить паттерн проектирования Абстрактная Фабрика
     public abstract class AbstractModel
     {
     }
--- WowApp.Database/AbstractRepository.cs
+++ WowApp.Database/AbstractRepository.cs
@@ -2,6 +2,9 @@ using Microsoft.Extensions.Logging;
 
 namespace WowApp.Database
 {
+    // Автрактные классы нужны для того, чтобы выделить общий функционал для семейства классов.
+    // Так как репозитории работают с базой данных, в этом абстрактном классе будет описана логика запросов к базе
+    // Например, сохранить нашу модельку. Моделек может быть много, а логика по сохранению модельки одна и та же.
     public abstract class AbstractRepository<T> where T : AbstractModel
     {
         protected ILogger<T> Logger;
