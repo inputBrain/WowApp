36990da1fb4a6c8b1ea84137ad0e7f7212abd8da
Alexander <vsany@meta.ua>
2021-08-28 06:39:02 +0300

    [!] interface fix

--- WowApp.Host/Interfaces/IWeapon.cs
+++ WowApp.Host/Interfaces/IWeapon.cs
@@ -8,7 +8,7 @@ namespace WowApp.Host.Interfaces
 {
     interface IWeapon
     {
-        public abstract string Name { get; }
+        public abstract string Name { get; set; }
         public abstract int Damage { get; }
         public abstract string Description { get; }
         void Fire();
