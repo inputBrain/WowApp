06bb0dffebb50dd0428f4dd7708c12c825532171
Alexander <vsany@meta.ua>
2021-08-30 00:48:15 +0300

    [+] Host controller

---
+++ WowApp.Host/Controllers/AbstractBaseController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WowApp.Host.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class AbstractBaseController<T> : ControllerBase
+    {
+        protected readonly ILogger<T> Logger;
+
+
+        protected AbstractBaseController(ILogger<T> logger)
+        {
+            Logger = logger;
+        }
+
+
+        protected IActionResult SendOk(object response)
+        {
+            return Ok(response);
+        }
+
+        protected IActionResult SendOk()
+        {
+            return Ok();
+        }
+
+    }
+}
--- WowApp.Host/Controllers/CoreController.cs
+++ WowApp.Host/Controllers/CoreController.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WowApp.Database.Service;
+
 namespace WowApp.Host.Controllers
 {
-    public class CoreController
+    [ApiController]
+    [Produces("application/json")]
+    public class CoreController : AbstractBaseController<CoreController>
     {
-        
+        private readonly IServiceContainer _serviceContainer;
+
+        public CoreController(ILogger<CoreController> logger, IServiceContainer serviceContainer) : base(logger)
+        {
+            _serviceContainer = serviceContainer;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_serviceContainer.UserService.GetTitleMessage());
+        }
     }
 }
