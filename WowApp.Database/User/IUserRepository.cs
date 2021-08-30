ce1c60aa975e5eb2c8558308e3a6a3472902cad0
JustAskIfYouDontKnow <gold.like.simple@gmail.com>
2021-08-31 00:28:34 +1000

    Comments for Slava

--- WowApp.Database/User/IUserRepository.cs
+++ WowApp.Database/User/IUserRepository.cs
@@ -2,9 +2,10 @@ using WowApp.Model.User;
 
 namespace WowApp.Database.User
 {
-    public interface IUserRepository
+    public interface IUserRepository//Абстрактное описание User'a
     {
-        UserModel Create(
+        UserModel Create
+            (
             string firstName,
             string lastName,
             string cover,
